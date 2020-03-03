5fdb2fac3e4e9e3ebc33d1e0c05e6e715662b1d0
Iron Fangs <39316701+frc5986@users.noreply.github.com>
2020-03-03 15:26:26 -0600

    Fixed some RPMs

--- src/main/java/frc/robot/autonomous/paths/ShootNMove.java
+++ src/main/java/frc/robot/autonomous/paths/ShootNMove.java
@@ -10,7 +10,7 @@ public class ShootNMove extends CommandGroup {
     public ShootNMove() {
 
         /* Shoots balls at 1580 RPM, estimated initial speed of 34% */
-        addSequential(new AutoShoot(1800, 0.39));
+        addSequential(new AutoShoot(1800, 0.43));
         //addParallel(new WaitCommand(13));
         /* Drives back for 6ft 0in */
         addSequential(new StraightDrive(false, 6, 0, false));
